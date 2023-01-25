93c2971f0fc88fd69fbc0973de4798e0c5ae502a
Johnny Squardo <johnnysquardo@gmail.com>
2023-01-25 16:16:46 -0500

    init dev

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,29 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import { Links, LiveReload, Outlet } from "@remix-run/react";
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+import globalStylesUrl from "./styles/global.css";
+
+export const links: LinksFunction = () => {
+	return [
+		{
+			rel: "stylesheet",
+			href: globalStylesUrl,
+		},
+	];
+};
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<title>Remix: So great, it's funny!</title>
+				<Links />
+			</head>
+			<body>
+				<Outlet />
+				<LiveReload />
+			</body>
+		</html>
+	);
 }
